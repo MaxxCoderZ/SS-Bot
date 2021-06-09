9b2e9d32fdc6f56b6363cf9427551a5eca8e2257
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-06-09 08:39:10 +0530

    Update about.py

--- bot/plugins/about.py
+++ bot/plugins/about.py
@@ -1,3 +1,9 @@
+from pyrogram import Filters, InlineKeyboardMarkup, InlineKeyboardButton
+
+from ..screenshotbot import ScreenShotBot
+
+
+ABOUT_TEXT = """
 <b>A screeshot generator bot with your custom watermark, also support sample vedio generating system
 
 Name : Screenshot Bot
@@ -11,3 +17,14 @@ Server : heroku
 Developer : @Maxxcoderz
 
 And if you want to add any other Modes, request it in @CoderzSupport</b>
+"""
+
+
+
+@ScreenShotBot.on_message(Filters.private & Filters.command("about"))
+async def help(c, m):
+    
+    await m.reply_text(
+        text=ABOUT_TEXT.format(m.from_user.first_name),
+        quote=True
+    )
