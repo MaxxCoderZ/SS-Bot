0451e7d934fb9a71f76c7e479c04242d8e39b0b3
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-06-09 08:37:26 +0530

    Create about.py

---
+++ bot/plugins/about.py
@@ -0,0 +1,13 @@
+<b>A screeshot generator bot with your custom watermark, also support sample vedio generating system
+
+Name : Screenshot Bot
+
+Language : python
+
+Framework : pyrogram
+
+Server : heroku
+
+Developer : @Maxxcoderz
+
+And if you want to add any other Modes, request it in @CoderzSupport</b>
